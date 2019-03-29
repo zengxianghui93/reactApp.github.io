41403da1c997d585d0756e8c25e33f6dbdc6d815
zengxianghui <zengxianghui@wecash.net>
2019-03-29 13:32:39 +0800

    build project

---
+++ build/precache-manifest.f60e78e016596fa8129be560b5143da2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "93344e886878eb8eaf67",
+    "url": "./static/js/main.db02b3df.chunk.js"
+  },
+  {
+    "revision": "a74c155c58b0a864c1ea",
+    "url": "./static/js/2.6c604cc8.chunk.js"
+  },
+  {
+    "revision": "93344e886878eb8eaf67",
+    "url": "./static/css/main.980c7e94.chunk.css"
+  },
+  {
+    "revision": "a74c155c58b0a864c1ea",
+    "url": "./static/css/2.9ffd3742.chunk.css"
+  },
+  {
+    "revision": "f5956f37828b84fe1e4f4be46f42af98",
+    "url": "./index.html"
+  }
+];
